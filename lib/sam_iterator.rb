4316612881fcf3e5cdf2d30a9a3631aaf5ab1d83
Ben Woodcroft <donttrustben@gmail.com>
2013-07-04 14:21:13 +1000

    sam_iterator

---
+++ lib/sam_iterator.rb
@@ -0,0 +1,48 @@
+module Bio
+  class SamIterator
+    class SamAlignment
+      attr_accessor :qname, :flag, :rname,:pos,:mapq,:cigar, :mrnm, :mpos, :isize, :seq, :qual
+
+      def initialize(sam)
+        s = sam.split("\t")
+        self.qname = s[0]
+        self.flag  = s[1].to_i
+        self.rname = s[2]
+        self.pos   = s[3].to_i
+        self.mapq  = s[4].to_i
+        self.cigar = s[5]
+        self.mrnm  = s[6]
+        self.mpos  = s[7].to_i
+        self.isize = s[8].to_i
+        self.seq   = s[9]
+        self.qual =  s[10]
+      end
+    end
+
+    class AlignmentSet < Array
+    end
+
+    def initialize(io)
+      @io = io
+    end
+
+    def each_alignment_set(&block)
+      last_alignment_name = nil
+      current_set = nil
+      @io.each_line do |line|
+        aln = SamAlignment.new(line.chomp)
+        if last_alignment_name != aln.qname
+          unless current_set.nil?
+            yield current_set
+            current_set = AlignmentSet.new
+          end
+        end
+        current_set.push aln
+      end
+      # yield the last set, if there is one
+      yield current_set unless current_set.nil?
+      nil
+    end
+  end
+end
+
--- lib/sra_16S_scripts.rb
+++ lib/sra_16S_scripts.rb
@@ -1,1 +1,3 @@
 require 'bio-sra_fastq_dumper/sra_fastq_dumper'
+
+require 'sam_iterator'
